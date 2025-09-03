a18daf945511b98a688363f87a4a53d843e35999
Qixuqi <qxuqi28@gmail.com>
2025-09-03 18:56:41 +1200

    配置tremor+Tailwind

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Card, Title, Text } from "@tremor/react";
 
 const Home = () => {
+  const [list, setList] = React.useState([]);
+  useEffect(() => {
+    fetch("/api/transactions?type=income").then((res) => {
+      res.json().then((data) => {
+        debugger;
+        setList(data);
+      });
+    });
+  }, []);
   return (
-    <div>
-      Home
+    <div className="p-6">
+      <Card>
+        <Title>Welcome</Title>
+        <Text>Now using Tremor + Tailwind.</Text>
+      </Card>
+      {list.map((item) => (
+        <div key={item.id}>
+          {item.id} - {item.amount} - {item.type}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
