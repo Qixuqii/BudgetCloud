9f033279b093a86c5a2d3c5586be09214a7e26af
Qixuqi <qxuqi28@gmail.com>
2025-09-20 17:41:00 +1200

    谷歌登录下载

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -2,30 +2,31 @@ import { useEffect, createContext, useState } from "react";
 import axios from "axios";
 // import { Children } from "react";
 
- export const AuthContext = createContext();
+export const AuthContext = createContext();
 
- export const AuthContextProvider  = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
 
-    const login = async (inputs)=>{
-        const res = await axios.post("/api/auth/login", inputs);
-        //await 是等待浏览器向后端发送请求并等待服务器处理并回应相应例如{“id”：1}然后把这个变成res
-        // 不然后面的res.data就会undefined
-        setCurrentUser(res.data);
-    }
+  const login = async (inputs) => {
+    const res = await axios.post("/api/auth/login", inputs);
+    //await 是等待浏览器向后端发送请求并等待服务器处理并回应相应例如{“id”：1}然后把这个变成res
+    // 不然后面的res.data就会undefined
+    setCurrentUser(res.data);
+  };
 
-    const logout = async (inputs)=>{
-        await axios.post("/api/auth/logout");
-        setCurrentUser(null);
-    }
+  const logout = async (inputs) => {
+    await axios.post("/api/auth/logout");
+    setCurrentUser(null);
+  };
 
-    useEffect(() => {
-        localStorage.setItem("user", JSON.stringify(currentUser));
-    },[currentUser]);
-    return (
-        <AuthContext.Provider value={{ currentUser, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
-
- };
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css';
-import { AuthContextProvider } from './context/authContext.jsx'
-import { Provider } from 'react-redux'
-import store from './store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthContextProvider } from "./context/authContext.jsx";
+import { Provider } from "react-redux";
+import store from "./store";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </Provider>
-  </React.StrictMode>,
-)
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <Provider store={store}>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </Provider>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
