c3c25a39437d937f39bd75ad144cb7685ff31f0d
Ubuntu <ubuntu@ip-172-31-31-243.ec2.internal>
2025-10-06 09:11:26 +0000

    chore: update client vite config, api/package.json and add pm2 ecosystem config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+	  host: '0.0.0.0',
+	  port: 80,
+	  strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8800',
-        // changeOrigin: true,
+        changeOrigin: true,
         // rewrite: path => path.replace(/^\/api/, ''),
       },
     },
