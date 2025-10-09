4335e6a4c120875bbfd32bcaaf61446c28cc1ec3
Ubuntu <ubuntu@ip-172-31-31-243.ec2.internal>
2025-10-09 07:50:58 +0000

    修改client的vite config里的域名

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
 	  host: '0.0.0.0',
 	  port: 80,
 	  strictPort: true,
+	  allowedHosts: ['budget.y2ki.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:8800',
